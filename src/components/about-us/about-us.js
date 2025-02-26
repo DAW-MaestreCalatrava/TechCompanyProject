ea48a8c426c065eea051f9195072ca747a895f28
baraja25 <92746513+baraja25@users.noreply.github.com>
2025-02-26 20:48:43 +0100

    feat: add About Us component with styling and content to index.html

---
+++ src/components/about-us/about-us.js
@@ -0,0 +1,41 @@
+import styles from './about-us.css' with { type: 'css' };
+
+class AboutUs extends HTMLElement {
+    
+    static get observedAttributes() {
+        return ['title', 'src'];
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.title = this.getAttribute('title') || 'Título';
+        this.src = this.getAttribute('src') || 'https://via.placeholder.com/150';
+        this.shadowRoot.adoptedStyleSheets = [styles];
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = /* html */`
+            <div class="aboutContainer">
+                <div class="about--textContainer">
+                    <my-text text size="title" color="#7f1b7e">${this.title}</my-text>
+                        <div class="about--text">
+                            <slot name="description"></slot>
+                        </div>
+                </div>
+                <img src="${this.src}" alt="about us" class="about--img"/>
+            </div>
+        `;
+    }
+
+    attributeChangedCallback() {
+        this.render();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+}
+
+customElements.define('about-us', AboutUs);
